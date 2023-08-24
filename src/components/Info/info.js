1953e874fcbd3cfad52ca6b1df1bec886386a074
Nhoj <nhojplummer@yahoo.com>
2023-08-24 15:11:22 -0400

    Testing a new idea

--- src/components/Info/info.js
+++ src/components/Info/info.js
@@ -7,9 +7,13 @@ import mobileImg from "../../assets/image-header-mobile.jpg"
 const Info = () => {
   return (
     <Section>
-      <div className="info-text-container"></div>
+      <div className="info-text-container">
+        <h2 className="info-text-header">Get insights that help your business grow.</h2>
+        <p className="info-text-description"></p>
+        <ul className="info-text_numbers-container"></ul>
+      </div>
       <div className="info-image-container">
-        <img src={desktopImg} srcSet={`${mobileImg} 375w, ${desktopImg} 1080w`} alt="desktopimg" />
+        {/* <img src={desktopImg} srcSet={`${mobileImg} 375w, ${desktopImg} 1080w`} alt="desktopimg" /> */}
       </div>
     </Section>
   )
