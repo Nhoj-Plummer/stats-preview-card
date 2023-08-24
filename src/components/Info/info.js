cbe7ad24c400f2a35db3293528295c2b3d65efff
Nhoj <nhojplummer@yahoo.com>
2023-08-24 14:48:55 -0400

    Making progress

--- src/components/Info/info.js
+++ src/components/Info/info.js
@@ -1,9 +1,18 @@
 import React from "react"
 import "./info.css"
 import Section from "../Section/section"
+import desktopImg from "../../assets/image-header-desktop.jpg"
+import mobileImg from "../../assets/image-header-mobile.jpg"
 
 const Info = () => {
-  return <Section>INfo</Section>
+  return (
+    <Section>
+      <div className="info-text-container"></div>
+      <div className="info-image-container">
+        <img src={desktopImg} srcSet={`${mobileImg} 375w, ${desktopImg} 1080w`} alt="desktopimg" />
+      </div>
+    </Section>
+  )
 }
 
 export default Info
