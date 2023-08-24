96287d4be5af8ec6bb5e563b678414d29ef4fa6c
Nhoj <nhojplummer@yahoo.com>
2023-08-24 19:00:41 -0400

    Making progress

--- src/components/Info/info.js
+++ src/components/Info/info.js
@@ -8,17 +8,23 @@ const Info = () => {
   return (
     <Section>
       <div className="info-text-container">
-        <h2 className="info-text-header">Get insights that help your business grow.</h2>
+        <h2 className="info-text-header">
+          Get <span>insights</span> that help your business grow.
+        </h2>
         <p className="info-text-description">Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and over efficiency.</p>
         <ul className="info-text_numbers-container">
-          <li className="info-text-number-data">10k+<span>Companies</span></li>
-          <li className="info-text-number-data">314<span>Templates</span></li>
-          <li className="info-text-number-data">12M+<span>Queries</span></li>
+          <li className="info-text-number-data">
+            10k+<span>Companies</span>
+          </li>
+          <li className="info-text-number-data">
+            314<span>Templates</span>
+          </li>
+          <li className="info-text-number-data">
+            12M+<span>Queries</span>
+          </li>
         </ul>
       </div>
-      <div className="info-image-container">
-        {/* <img src={desktopImg} srcSet={`${mobileImg} 375w, ${desktopImg} 1080w`} alt="desktopimg" /> Method to swithc imgages depending on size  */}
-      </div>
+      <div className="info-image-container">{/* <img src={desktopImg} srcSet={`${mobileImg} 375w, ${desktopImg} 1080w`} alt="desktopimg" /> Method to swithc imgages depending on size  */}</div>
     </Section>
   )
 }
