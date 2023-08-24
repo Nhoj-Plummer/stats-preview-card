5d9cdf42008e345be89c98be53ca7d458365fa3d
Nhoj <nhojplummer@yahoo.com>
2023-08-24 16:46:16 -0400

    Finally got the responsiveness

--- src/components/Info/info.js
+++ src/components/Info/info.js
@@ -1,8 +1,8 @@
 import React from "react"
 import "./info.css"
 import Section from "../Section/section"
-import desktopImg from "../../assets/image-header-desktop.jpg"
-import mobileImg from "../../assets/image-header-mobile.jpg"
+// import desktopImg from "../../assets/image-header-desktop.jpg"
+// import mobileImg from "../../assets/image-header-mobile.jpg"
 
 const Info = () => {
   return (
@@ -10,10 +10,14 @@ const Info = () => {
       <div className="info-text-container">
         <h2 className="info-text-header">Get insights that help your business grow.</h2>
         <p className="info-text-description">Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and over efficiency.</p>
-        <ul className="info-text_numbers-container"></ul>
+        <ul className="info-text_numbers-container">
+          <li className="info-text-number-data">10k+<span>Companies</span></li>
+          <li className="info-text-number-data">314<span>Templates</span></li>
+          <li className="info-text-number-data">12M+<span>Queries</span></li>
+        </ul>
       </div>
       <div className="info-image-container">
-        {/* <img src={desktopImg} srcSet={`${mobileImg} 375w, ${desktopImg} 1080w`} alt="desktopimg" /> */}
+        {/* <img src={desktopImg} srcSet={`${mobileImg} 375w, ${desktopImg} 1080w`} alt="desktopimg" /> Method to swithc imgages depending on size  */}
       </div>
     </Section>
   )
